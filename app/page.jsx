a25865c8c62cbc63fb66f33ea8f6d561634cbc43
Enamulali <enamul97@icloud.com>
2023-10-29 21:36:24 +0000

    font styling update

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,13 @@ export default function Home() {
   return (
     <main>
       <h2>Home</h2>
+      <p>
+        {" "}
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit iste
+        reiciendis adipisci similique maiores excepturi deserunt exercitationem
+        esse rem laboriosam, corporis, unde assumenda quis quae eius suscipit
+        dignissimos doloribus laborum.
+      </p>
     </main>
   );
 }
