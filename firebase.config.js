034d86360096d17f452f80296198861a21b1a8e7
Enamulali <enamul97@icloud.com>
2023-10-29 15:01:58 +0000

    firebase setup

---
+++ firebase.config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBmuiUzBcR9wmUG4tVrzfICRKhWSHidTf8",
+  authDomain: "budgetting-app-64add.firebaseapp.com",
+  projectId: "budgetting-app-64add",
+  storageBucket: "budgetting-app-64add.appspot.com",
+  messagingSenderId: "932092613533",
+  appId: "1:932092613533:web:9051996969f5a56dae5574",
+  measurementId: "G-8S3B48V2HK",
+};
+
+const app = initializeApp(firebaseConfig);
